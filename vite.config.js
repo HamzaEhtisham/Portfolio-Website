447ab060d7f13b0da256e015e3fec5b9cd53c8e6
HamzaEhtisham <hamzaehtisham124@gmail.com>
2025-09-04 11:10:29 +0500

    fix: allowedHosts for render

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    allowedHosts: [
+      "portfolio-website-nh9r.onrender.com", // apna Render domain add kiya
+    ],
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
-})
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
